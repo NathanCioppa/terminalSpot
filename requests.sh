bc226b9dd08395700eae80c3a0d1e0b5b1b84c3b
NathanCioppa <nathan.cioppa@gmail.com>
2025-06-10 15:29:19 -0400

    general get request function

--- requests.sh
+++ requests.sh
@@ -7,12 +7,31 @@ authBear="Authorization: Bearer"
 accessToken=$(refreshAccessToken)
 
 function getUserInfo() {
-	response=$(curl -s -w "\n%{http_code}" -X GET \
-  	--url "$baseEndpoint"/me \
-  	--header "$authBear $accessToken"
-	)
+	response=$(requestGet "/me")
 
 	echo "$response"
+}
+
+function requestGet() {
+	endpoint="$1"
+	response=$(curl -s -w "\n%{http_code}" -X GET \
+		--url "$baseEndpoint$endpoint" \
+		--header "$authBear $accessToken"
+	)
+	httpCode=$(echo "$response" | tail -n1)
+	body=$(echo "$response" | sed '$d')
+
+	if [[ "$httpCode" -eq 200 ]]; then
+		# echo body only if it is valid JSON
+		if jq -e . >/dev/null 2>&1 <<< "$body"; then
+			echo "$body"
+			exit 0
+		else
+			exit 1
+		fi
+	else # the request was unsuccessful
+		exit 1
+	fi
 }
 
 getUserInfo
