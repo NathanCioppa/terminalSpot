4e86a56aaa3623d68875331d2cd540f3acdf027e
NathanCioppa <nathan.cioppa@gmail.com>
2025-06-19 18:13:25 -0400

    username display in ncurses

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <ncurses.h>
+#include <sys/wait.h>
+
+int drawName(int x, int y);
+
+int main() {
+	initscr();
+	raw();
+	drawName(0,0);
+	refresh();
+	getch();
+	endwin();
+	return 0;
+}
+
+int drawName(int x, int y) {
+	FILE *fp = popen("./requests/getUserInfo", "r");
+	char buffer[31]; // maximum allowed display name by spotify is 30 characters, 31 allows space for terminating char.
+
+	if (fp) {
+		char *result = fgets(buffer, sizeof(buffer), fp);
+		int status = pclose(fp);
+		// fgets fails, or the command exits abnormally, or exits with status 1; all indicate a failed execution.
+		if(result == NULL || !WIFEXITED(status) || WEXITSTATUS(status))
+			return 0;
+
+		move(x,y);
+		printw("%s", buffer);
+
+		return 1;
+	}
+	return 0;
+}
