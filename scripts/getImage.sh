732cdbe91ca0c4a14cb58be6433674351b6c7eff
NathanCioppa <nathan.cioppa@gmail.com>
2025-06-22 13:17:33 -0400

    getCurrentPlaying prints info as new line separated list

--- scripts/getImage.sh
+++ scripts/getImage.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 # expects some valid json object with an album array in it
 
 function getAlbumImageUrl() {
 	echo "$1" | jq -r '.album.images[0].url' || echo "default image url here"
+}
+
+function getFirstImageUrl() {
+	echo "$1" | jq -r '.images[0].url' || echo "default image url here"
+
 }
---
+++ scripts/newLineList.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function newLineList() {
+	[ "$#" -eq 0 ] && echo "Expected at least one argument" && exit 1
+	list=""
+	for arg in "$@"; do
+		list+="${arg}"$'\n'
+	done
+	printf "%s" "${list}" # trim final new line char
+}
